2fe6d05853c17fbc3e283c3fef98693de9565d05
baldomeroOsorio <bosorio1117@gmail.com>
2023-06-15 16:12:48 -0500

    Añadir clase A con atributos foo y bar

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+    class A {
+    private $foo;
+    private $bar;
+
+    public function getFoo() {
+        return $this->foo;
+    }
+
+    public function setFoo($foo) {
+        $this->foo = $foo;
+    }
+
+    public function getBar() {
+        return $this->bar;
+    }
+
+    public function setBar($bar) {
+        $this->bar = $bar;
+    }
+}
