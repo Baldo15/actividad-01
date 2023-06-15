a37b6754ca0ac2f2167a577ee0afccde55ac3f15
baldomeroOsorio <bosorio1117@gmail.com>
2023-06-15 16:46:14 -0500

    Añadir clase B

--- index.php
+++ index.php
@@ -1,21 +1,41 @@
 <?php 
     class A {
-    private $foo;
-    private $bar;
+        private $foo;
+        private $bar;
 
-    public function getFoo() {
-        return $this->foo;
-    }
+        public function getFoo() {
+            return $this->foo;
+        }
 
-    public function setFoo($foo) {
-        $this->foo = $foo;
-    }
+        public function setFoo($foo) {
+            $this->foo = $foo;
+        }
 
-    public function getBar() {
-        return $this->bar;
-    }
+        public function getBar() {
+            return $this->bar;
+        }
 
-    public function setBar($bar) {
-        $this->bar = $bar;
+        public function setBar($bar) {
+            $this->bar = $bar;
+        }
     }
-}
+    class b {
+        private $foo;
+        private $bar;
+
+        public function getFoo() {
+            return $this->foo;
+        }
+
+        public function setFoo($foo) {
+            $this->foo = $foo;
+        }
+
+        public function getBar() {
+            return $this->bar;
+        }
+
+        public function setBar($bar) {
+            $this->bar = $bar;
+        }
+    }
